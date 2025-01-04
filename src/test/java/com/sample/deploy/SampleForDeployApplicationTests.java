006f376f55fa09ea4b84c4d4f069b015858d66b5
Deepak Mittal <deepak.mittal@prysomsystems.com>
2025-01-04 15:40:54 +0530

    first commit

---
+++ src/test/java/com/sample/deploy/SampleForDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleForDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
