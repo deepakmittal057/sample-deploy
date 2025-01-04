b7038f51e296e0b67ef95ff89bffc260d1954e00
Deepak Mittal <deepak.mittal@prysomsystems.com>
2025-01-04 16:02:24 +0530

    added new controller

---
+++ src/main/java/com/sample/deploy/controllers/MainController.java
@@ -0,0 +1,19 @@
+package com.sample.deploy.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+
+    @GetMapping
+    public String getRes(){
+        return "Hello this is spring project";
+    }
+
+}
